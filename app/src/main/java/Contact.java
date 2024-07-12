e6d97882a5df3fece25aae301f3f55412b69bc8d
arshedgithub <arshedahmedmail@gmail.com>
2024-07-12 10:39:33 +0530

    add Contact java class

---
+++ app/src/main/java/Contact.java
@@ -0,0 +1,19 @@
+public class Contact {
+
+    private String contactName;
+    private int imageId;
+
+    public Contact(String contactName, int imageId) {
+        this.contactName = contactName;
+        this.imageId = imageId;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+}
