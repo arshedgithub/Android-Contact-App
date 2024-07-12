9df9b53a01f18e39c26535bb751f24fa117c5357
arshedgithub <arshedahmedmail@gmail.com>
2024-07-12 10:46:35 +0530

    make image round

--- app/src/main/java/com/example/adapetertest/MainActivity.java
+++ app/src/main/java/com/example/adapetertest/MainActivity.java
@@ -8,12 +8,12 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<Contact> contactArrayList;
+//    private ArrayList<Contact> contactArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
-        
+
     }
 }
